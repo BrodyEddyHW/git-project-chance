f8b133ae1f788e58f89b6809e702f658d127f319
Chance Krenzer <ckrenzer1@hwemail.com>
2024-09-20 12:27:30 -0700

    edited deleteRepo()
    made deleteRepo() void and printed out instead

--- Git.java
+++ Git.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.Writer;
 
 public class Git {
+    public static void main(String[] args) throws IOException {
+        initRepo();
+    }
     public static void initRepo () throws IOException {
         if (new File("git/objects").exists() && new File("git", "index").exists()) {
             System.out.println("Git Repository already exists");
@@ -12,17 +15,17 @@ public class Git {
             new File("git/objects").mkdirs();
             File directory = new File ("git");
             File index = new File(directory, "index");
-            FileWriter writer = new FileWriter(index);
+            index.createNewFile();
         }
     }
-    private String deleteRepo() {
+    private static void deleteRepo() {
         if ((new File("git/objects").exists() && new File("git", "index").exists())) {
             File index = new File("git", "index");
             File objects = new File("git/objects");
             File gitDirectory = new File("git");
             index.delete(); objects.delete(); gitDirectory.delete();
-            return "Git Repository deleted";
+            System.out.println("Git Repository deleted");
         }
-        return "Git Repository does not exist";
+        System.out.println("Git Repository does not exist");
     }
 }
