5427d6f817e6fa4b45701bf22382b2de2b9a6e2f
Chance Krenzer <ckrenzer1@hwemail.com>
2024-09-20 10:53:00 -0700

    Added deleteRepo() and edited .gitignore
    added deleteRepo() which removes the objects dir, index file and the git dir. Also edited .gitignore because .DS_Store was not working

--- Git.java
+++ Git.java
@@ -4,19 +4,25 @@ import java.io.IOException;
 import java.io.Writer;
 
 public class Git {
-    public static void main(String[] args) throws IOException {
-        initRepo();
-    }
     public static void initRepo () throws IOException {
-        if (new File("git/objects").exists()) {
-            System.out.println("Repository already exists.");
+        if (new File("git/objects").exists() && new File("git", "index").exists()) {
+            System.out.println("Git Repository already exists");
         }
         else {
             new File("git/objects").mkdirs();
             File directory = new File ("git");
             File index = new File(directory, "index");
-            Writer writer = new FileWriter(index);
-            
+            FileWriter writer = new FileWriter(index);
         }
+    }
+    private String deleteRepo() {
+        if ((new File("git/objects").exists() && new File("git", "index").exists())) {
+            File index = new File("git", "index");
+            File objects = new File("git/objects");
+            File gitDirectory = new File("git");
+            index.delete(); objects.delete(); gitDirectory.delete();
+            return "Git Repository deleted";
+        }
+        return "Git Repository does not exist";
     }
 }
